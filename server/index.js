504c74ff5650b6a93f47129992c5255f94796a5a
Therese Brown <browntm96@gmail.com>
2019-09-12 20:12:32 -0700

    Cleaning up code

--- server/index.js
+++ server/index.js
@@ -7,10 +7,6 @@ app.use(express.static(__dirname + '/../public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('Made it!');
-});
-
 app.listen(port, () => {
   console.log(`App listening on ${port}`);
 });
