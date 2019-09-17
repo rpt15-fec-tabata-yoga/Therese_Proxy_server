6e595872f688b8d3cca5de5680ba20a8929cec79
Therese Brown <browntm96@gmail.com>
2019-09-17 15:03:53 -0700

    Add another app.use

--- server/index.js
+++ server/index.js
@@ -4,8 +4,8 @@ const cors = require('cors');
 const app = express();
 const port = 3006;
 
-app.use(express.static(__dirname + '/../public'));
-app.use('/:gameId', express.static('public'));
+app.use('/', express.static(__dirname + '/../public'));
+app.use('/:gameId', express.static(__dirname + '/../public'));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
